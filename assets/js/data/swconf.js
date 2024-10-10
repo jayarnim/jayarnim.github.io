63756f0b22d4e827927adbe14bb429addc201e20
jayarnim <jayarnim@users.noreply.github.com>
2024-10-10 04:11:16 +0000

    deploy: e111504b7c100b29cfe40c9ae4e34ce8483ad143

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728528527',resources: [
+    cacheName: 'chirpy-1728533472',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
